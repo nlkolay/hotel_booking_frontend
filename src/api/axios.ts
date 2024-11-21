5c4efaae08d6845a069463be5835f74b9dc134ad
nlkolay <klushka50@gmail.com>
2024-11-21 10:45:02 +0300

    "Update baseURL in axios instance from 'https://booking-app-7ug2.onrender.com/' to 'http://103.13.211.15/'"

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-baseURL: "https://booking-app-7ug2.onrender.com/",
+baseURL: "http://103.13.211.15/",
 // "http://localhost:8000/" // for local development
-// "https://booking-app-7ug2.onrender.com/"  // for production
+// "https://booking-app-7ug2.onrender.com/"  // for production on render
+// "http://103.13.211.15/" // for production on seege
 withCredentials: true,
 headers: {
 "Content-Type": "application/json",
